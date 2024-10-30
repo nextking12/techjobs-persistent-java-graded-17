9e0f24fdd9d0c57564c9ec887d789c56926f3809
Edward King <king8three@gmailcom>
2024-10-30 15:52:55 -0400

    Task 4 submittal

--- queries.sql
+++ queries.sql
@@ -1,14 +1,20 @@
 --Part 1
---id INT
---name VARCHAR(255)
---employer VARCHAR(255)
---skills VARCHAR(255)
+id INT
+name VARCHAR(255)
+employer VARCHAR(255)
+skills VARCHAR(255)
 
 
 --Part 2
-select name from employer
-where location = "St. Louis City";
+SELECT name
+FROM employer
+WHERE location = "St. Louis City";
 
 --Part 3
+DROP TABLE job;
 
 --Part 4
+SELECT * FROM skill
+LEFT JOIN job_skills ON skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -2,8 +2,12 @@ package org.launchcode.techjobs.persistent.models;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Skill extends AbstractEntity {
@@ -11,6 +15,9 @@ public class Skill extends AbstractEntity {
     @NotBlank
     @Size(min=3, max=50)
     private String description;
+
+    @ManyToMany(mappedBy = "skills")
+    private List<Job> jobs = new ArrayList<>();
 
     public Skill(String description) {
         this.description = description;
@@ -25,4 +32,13 @@ public class Skill extends AbstractEntity {
     public void setDescription(String description) {
         this.description = description;
     }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
 }
+
